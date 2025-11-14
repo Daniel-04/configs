7ac09b0e39b7e8c07c9c1438663b3e5678b4b305
Daniel <danielelacer08@gmail.com>
2025-11-14 19:39:04 +0000

    clang, ghc, git, + install

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+cp bashrc ~/.bashrc
+cp clang-format ~/.clang-format
+cp -r doom ~/.config/
+cp ghci ~/.ghci
+cp gitconfig ~/.gitconfig
+cp starship.toml ~/.config/starship.toml
